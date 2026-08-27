ccfafc74304aacf189223564fc65297d5ee44c55
Chris Leonavicius <chris.leonavicius@amplitude.com>
2026-08-27 08:59:07 -0700

    build: raise Swift 6.4 iOS deployment target

--- Package@swift-6.4.swift
+++ Package@swift-6.4.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "analytics-connector-ios",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v15),
         .macOS(.v12),
         .tvOS(.v15),
         .watchOS(.v9),
